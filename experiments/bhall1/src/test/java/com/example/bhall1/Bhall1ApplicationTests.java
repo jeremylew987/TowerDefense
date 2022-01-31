6d4cde91cbaf0e07b30600cea974bb0009107357
unknown <swidexx@gmail.com>
2022-01-31 14:37:29 -0600

    springboot initialized for experiment

---
+++ experiments/bhall1/src/test/java/com/example/bhall1/Bhall1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bhall1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bhall1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
