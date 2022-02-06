af305d3a6fd0f3579c73e8b8365835c546c782d3
benhall <benhall@iastate.edu>
2022-02-06 03:19:03 -0600

    Revert "fixing up git errors"
    This reverts commit 15a59b6bfcb36f3a88bd05f0b77144406c2fbffc

--- experiments/bhall1/src/main/java/coms309/login/bhall1/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package coms309.login.bhall1.repository;
-
-import coms309.login.bhall1.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
