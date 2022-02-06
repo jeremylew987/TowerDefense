c1f7f5fdc70d422d65b2da13621cbfd8b00ce592
Ben Hall <swidexx@gmail.com>
2022-02-06 02:46:25 -0600

    random fixes to clean

---
+++ experiments/bhall1/src/main/java/coms309/trivia/bhall1/Bhall1Application.java
@@ -0,0 +1,15 @@
+package coms309.trivia.bhall1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class Bhall1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bhall1Application.class, args);
+	}
+
+}
