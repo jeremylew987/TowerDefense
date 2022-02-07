50ff7e488396c24e72d0a3a074e1a98ed8d4e664
JLew <jlew@gmail.com>
2022-02-06 20:33:49 -0600

    android volley get command?

--- experiments/jalewis1/exp2/app/src/main/java/com/example/test/page2.java
+++ experiments/jalewis1/exp2/app/src/main/java/com/example/test/page2.java
@@ -2,10 +2,22 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class page2 extends AppCompatActivity {
 
@@ -14,6 +26,7 @@ public class page2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
         Button back = findViewById(R.id.button3);
+        TextView info = findViewById(R.id.textView3);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,6 +35,32 @@ public class page2 extends AppCompatActivity {
                 finish();
 
             }});
+String address = "";
+        RequestQueue queue = Volley.newRequestQueue(page2.this);
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, address, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                     info.setText(response.getString("name"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                error.printStackTrace();
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(page2.this);
+                alertDialogBuilder.setTitle("Error");
+                alertDialogBuilder.setMessage(error.getMessage());
+                alertDialogBuilder.setPositiveButton("Ok", null);
+                alertDialogBuilder.setNegativeButton("", null);
+                alertDialogBuilder.create().show();
+            }
+        });
+        queue.add(request);
+
 
     }
 }
