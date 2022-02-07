4063c56cac97bf0768e508faf29ee938d5af4f4f
tergav17 <gctersteeg@gmail.com>
2022-02-06 22:00:39 -0600

    Updated graphical experiment pattern

--- experiments/tergav17/exp1_libgdx/core/src/com/tergav17/exp1/Experiment.java
+++ experiments/tergav17/exp1_libgdx/core/src/com/tergav17/exp1/Experiment.java
@@ -7,25 +7,55 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Experiment extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	Texture img[] = new Texture[19];
+
+	float count = 0;
+	int frame = 0;
+
+	double x = 1000;
+	double y = 500;
+	double dx = 15;
+	double dy = 0;
+
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		for (int i = 0; i < 19; i++) img[i] = new Texture("boing" + i + ".png");
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+
+		count = count + 0.01F;
+		frame++;
+
+		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		/* begin bounce ops */
+
+		// update physics
+		x += dx;
+		y += dy;
+
+		// check constraints
+		dy = dy - 0.3;
+		if (y < 0.0) dy = Math.abs(dy);
+		if (x < 0.0) dx = Math.abs(dx);
+		if (x > 2220 - 256) dx = -Math.abs(dx);
+		if (y > 1080 - 512) dy = -1;
+
+		batch.draw(img[frame % 19], (int) x, (int) y, 256, 256);
+
+		/* end bounce ops */
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		for (int i = 0; i < 19; i++) img[i].dispose();
 	}
 }
