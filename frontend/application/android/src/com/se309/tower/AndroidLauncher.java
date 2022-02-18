f1b2041835c565b38112cc011616f24188b55ff6
tergav17 <gctersteeg@gmail.com>
2022-02-18 10:38:24 -0600

    Added the project again (fixed this time)

---
+++ frontend/application/android/src/com/se309/tower/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.se309.tower;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.se309.tower.TowerDefense;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TowerDefense(), config);
+	}
+}
