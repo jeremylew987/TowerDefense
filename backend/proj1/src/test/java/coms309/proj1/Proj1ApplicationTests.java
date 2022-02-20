75459b8590a6f0a6d00e7a49986a67a5250a8792
jminardi <jminardi@iastate.edu>
2022-02-20 00:27:53 -0600

    Create backend project

---
+++ backend/proj1/src/test/java/coms309/proj1/Proj1ApplicationTests.java
@@ -0,0 +1,15 @@
+package coms309.proj1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proj1ApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
