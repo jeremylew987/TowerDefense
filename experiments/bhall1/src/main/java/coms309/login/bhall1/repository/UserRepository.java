7fcd3f4f615c795aa6a32bb9713a4be05a10030a
Ben Hall <swidexx@gmail.com>
2022-02-20 04:00:19 -0600

    added data transfer object for registered users

--- experiments/bhall1/src/main/java/coms309/login/bhall1/repository/UserRepository.java
+++ experiments/bhall1/src/main/java/coms309/login/bhall1/repository/UserRepository.java
@@ -2,7 +2,11 @@ package coms309.login.bhall1.repository;
 
 import coms309.login.bhall1.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    @Query("SELECT u FROM User u where u.email = ?1")
+    Optional<User> findByEmail(String email);
 }
