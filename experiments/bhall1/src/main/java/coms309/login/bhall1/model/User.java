0f7dbebfcf683ae02da2600bc2f16b41b35c21e3
Ben Hall <swidexx@gmail.com>
2022-02-20 03:41:12 -0600

    added email field

--- experiments/bhall1/src/main/java/coms309/login/bhall1/model/User.java
+++ experiments/bhall1/src/main/java/coms309/login/bhall1/model/User.java
@@ -11,12 +11,17 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String email;
     private String username;
     private String password;
 
     public Long getId() {
         return id;
     }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
 
     public String getUsername() {
         return username;
