c7fdd7fa4913e49630b389c4b95ccc31b40386c8
jminardi <jminardi@iastate.edu>
2022-02-28 00:46:26 -0600

    Add logger

--- backend/proj1/src/main/java/coms309/proj1/login/LoginController.java
+++ backend/proj1/src/main/java/coms309/proj1/login/LoginController.java
@@ -1,6 +1,9 @@
 package coms309.proj1.login;
 
+import coms309.proj1.registration.RegistrationController;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,8 +15,11 @@ public class LoginController {
 
     private LoginService loginService;
 
+    private final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     @PostMapping
     public ResponseEntity<String> login(@RequestBody LoginRequest request) {
+        logger.info("Entered into Login Controller Layer");
         return loginService.login(request);
     }
 
