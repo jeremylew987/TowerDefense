a68833567133bf11e77610e35d8290ef78eb87da
jminardi <jminardi@iastate.edu>
2022-02-27 18:39:52 -0600

    Add import

--- backend/proj1/src/main/java/coms309/proj1/registration/RegistrationController.java
+++ backend/proj1/src/main/java/coms309/proj1/registration/RegistrationController.java
@@ -2,6 +2,7 @@ package coms309.proj1.registration;
 
 import coms309.proj1.login.LoginException;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
