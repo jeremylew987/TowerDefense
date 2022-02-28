126b7e868924d2f5e5c904e1ab0d30140bb1121b
jminardi <jminardi@iastate.edu>
2022-02-27 18:40:52 -0600

    Add default message

--- backend/proj1/src/main/java/coms309/proj1/exception/InvalidEmailException.java
+++ backend/proj1/src/main/java/coms309/proj1/exception/InvalidEmailException.java
@@ -3,7 +3,7 @@ package coms309.proj1.exception;
 public class InvalidEmailException extends RuntimeException
 {
 	public InvalidEmailException() {
-		super();
+		super("Email is not valid");
 	}
 	public InvalidEmailException(String message) {
 		super(message);
