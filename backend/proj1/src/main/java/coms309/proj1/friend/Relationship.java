1845ddbd3580c4e63cda4c00319df7dd9e34ad1d
jminardi <jminardi@iastate.edu>
2022-03-03 16:52:40 -0600

    implement relationship constructor and getter/setters

---
+++ backend/proj1/src/main/java/coms309/proj1/friend/Relationship.java
@@ -0,0 +1,73 @@
+package coms309.proj1.friend;
+
+import coms309.proj1.user.User;
+import org.apache.tomcat.jni.Time;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "relationship")
+public class Relationship {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "relationship_id")
+	private Integer relationshipId;
+
+	@Column(name = "created_date")
+	private LocalDateTime createdDate;
+
+	@ManyToOne
+	//@JoinColumn(name = "friend_id", referencedColumnName = "user_id")
+	private User friend;
+
+	@ManyToOne
+	//@JoinColumn(name = "owner_id", referencedColumnName = "user_id")
+	private User owner;
+
+	public Relationship() {
+		this.createdDate = LocalDateTime.now();
+		this.friend = null;
+		this.owner = null;
+
+	}
+	public Relationship(User friend) {
+		super();
+		this.friend = friend;
+	}
+
+	public Integer getId() {
+		return relationshipId;
+	}
+
+	public void setId(Integer relationshipId) {
+		this.relationshipId = relationshipId;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public User getOwner() {
+		return owner;
+	}
+
+	public void setOwner(User owner) {
+		this.owner = owner;
+	}
+
+	public User getFriend() {
+		return friend;
+	}
+
+	public void setFriend(User friend) {
+		this.friend = friend;
+	}
+}
