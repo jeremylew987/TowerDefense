973b9e546776308a153ab36e09ba400810c24ec0
jminardi <jminardi@iastate.edu>
2022-03-02 23:44:17 -0600

    Add friend attribute list to user

--- backend/proj1/src/main/java/coms309/proj1/user/User.java
+++ backend/proj1/src/main/java/coms309/proj1/user/User.java
@@ -1,5 +1,6 @@
 package coms309.proj1.user;
 
+import coms309.proj1.Friend.Friend;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import javax.persistence.*;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 @Getter
 @Setter
@@ -38,6 +40,9 @@ public class User implements UserDetails {
     private UserRole role;
     private Boolean locked;
     private Boolean enabled;
+
+    @OneToMany(mappedBy = "user")
+    public List<Friend> friends;
 
     public User(String username, String email, String password, UserRole role) {
         this.username = username;
