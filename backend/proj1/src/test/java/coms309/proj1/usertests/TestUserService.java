84372e002e88c6e34dc8e92066ff35c9c02289c1
jminardi <jminardi@iastate.edu>
2022-03-18 16:54:51 -0500

    Add loaduser and register tests

--- backend/proj1/src/test/java/coms309/proj1/usertests/TestUserService.java
+++ backend/proj1/src/test/java/coms309/proj1/usertests/TestUserService.java
@@ -1,6 +1,115 @@
 package coms309.proj1.usertests;
 
+import coms309.proj1.exception.EmailNotFoundException;
+import coms309.proj1.registration.token.ConfirmationToken;
+import coms309.proj1.registration.token.ConfirmationTokenService;
+import coms309.proj1.user.User;
+import coms309.proj1.user.UserRepository;
+import coms309.proj1.user.UserRole;
+import coms309.proj1.user.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 public class TestUserService
 {
+	@InjectMocks
+	UserService userService;
+
+	@Mock
+	UserRepository userRepository;
+
+	@Mock
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	@Mock
+	ConfirmationTokenService confirmationTokenService;
+
+	private static final String username = "James";
+	private static final String email = "james@iastate.edu";
+	private static final String password = "1234";
+	private static final String encryptedPassword = "4321";
+	private static final UserRole role = UserRole.USER;
+	private static final User user = new User(username, email, password, role);
+
+
+	@BeforeEach
+	public void init() {
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test
+	public void loadUserByUsernameTest() {
+		// Test username exists
+		when(userRepository.findByUsername(username)).thenReturn(Optional.of(user));
+
+		User user1 = (User) userService.loadUserByUsername(username);
+
+		assertEquals(username, user1.getUsername());
+		assertEquals(email, user1.getEmail());
+
+		// Test username does not exist
+		when(userRepository.findByUsername(username)).thenReturn(Optional.empty());
+		try {
+			userService.loadUserByUsername(username);
+		} catch (UsernameNotFoundException e) {
+			return;
+		}
+		fail();
+	}
+
+	@Test
+	public void loadUserByEmailTest() {
+		// Test email exists
+		when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
+
+		User user1 = (User) userService.loadUserByEmail(email);
+
+		assertEquals(username, user1.getUsername());
+		assertEquals(email, user1.getEmail());
+
+		// Test email does not exist
+		when(userRepository.findByEmail(email)).thenReturn(Optional.empty());
+		try {
+			userService.loadUserByEmail(email);
+		} catch (EmailNotFoundException e) {
+			return;
+		}
+		fail();
+	}
+
+	@Test
+	public void registerUserTest() {
+		ConfirmationToken confirmationToken =
+				mock(ConfirmationToken.class, withSettings().useConstructor
+						("11223344",
+						LocalDateTime.now(),
+						LocalDateTime.now().plusMinutes(15),
+						user)
+				);
+		when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+		when(userRepository.findByUsername(user.getUsername())).thenReturn(Optional.of(user));
+
+		when(bCryptPasswordEncoder.encode(password)).thenReturn(encryptedPassword);
+
+		//doNothing().when(userRepository).save(user);
+		when(userRepository.save(user)).thenReturn(user);
+		doNothing().when(confirmationTokenService).saveConfirmationToken(any(ConfirmationToken.class));
+		String token = userService.registerUser(user);
+		assertEquals("11223344", token);
+	}
 
 }
