09d665b424fe5b2254cdc606e11f2ffe956c7c5f
tergav17 <gctersteeg@gmail.com>
2022-03-18 01:27:31 -0500

    Added serialization and deserialization tests

--- frontend/application/android/src/com/se309/test/NetworkManagerTestBench.java
+++ frontend/application/android/src/com/se309/test/NetworkManagerTestBench.java
@@ -12,7 +12,15 @@ public class NetworkManagerTestBench {
 
         System.out.println("Testing serialization...");
 
-        System.out.println(nw.serialize("Hello, world"));
+        String serial = nw.serialize(new UserDummy("foo", "bar"));
+
+        System.out.println(serial);
+
+        System.out.println("Testing deserialization...");
+
+        UserDummy original = (UserDummy) nw.deserialize(serial, UserDummy.class);
+
+        System.out.println("Username: " + original.getUsername() + " Password: " + original.getPassword());
     }
 
 }
