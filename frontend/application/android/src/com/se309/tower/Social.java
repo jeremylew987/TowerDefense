227def6289bda60659cc959c5ff1c0b83f83f5fb
JLew <jlew@gmail.com>
2022-03-22 21:38:44 -0500

    work on the friends list recieving endpoint

--- frontend/application/android/src/com/se309/tower/Social.java
+++ frontend/application/android/src/com/se309/tower/Social.java
@@ -3,6 +3,7 @@ package com.se309.tower;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -10,6 +11,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class Social extends AppCompatActivity {
@@ -42,6 +51,31 @@ public class Social extends AppCompatActivity {
             }});
 
         //call for friends TODO loop through using friend function
+        RequestQueue queue = Volley.newRequestQueue(Social.this);
+        final SharedPreferences mPrefs = getSharedPreferences("test",0);
+        String usernameSave = mPrefs.getString("username","none");
+        String friendaddress =  "http://coms-309-027.class.las.iastate.edu:8080/friends/" + usernameSave;
+        JsonArrayRequest userClubs = new JsonArrayRequest(Request.Method.GET, friendaddress, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.i("Response: " , response.toString());
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        friend(response.getJSONObject(i));
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(userClubs);
         //same for freiendRquests
 
 
