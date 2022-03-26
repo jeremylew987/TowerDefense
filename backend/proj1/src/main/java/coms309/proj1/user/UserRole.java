87c6bec943d1610c13c8341543ffcd026d346f35
jminardi <jminardi@iastate.edu>
2022-03-26 16:43:36 -0500

    Removed auth class

--- backend/proj1/src/main/java/coms309/proj1/user/UserRole.java
+++ backend/proj1/src/main/java/coms309/proj1/user/UserRole.java
@@ -3,10 +3,6 @@ package coms309.proj1.user;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-public class help implements GrantedAuthority
-{
-    new SimpleGrantedAuthority
-}
 public enum UserRole
 {
     USER,
