7ee80d517774a79dffb0ceda9a862834cff8a536
jminardi <jminardi@iastate.edu>
2022-03-26 16:40:59 -0500

    Add admin and user endpoint. Temp remove registration

--- backend/proj1/src/main/java/coms309/proj1/user/UserController.java
+++ backend/proj1/src/main/java/coms309/proj1/user/UserController.java
@@ -1,6 +1,10 @@
 package coms309.proj1.user;
 
+import coms309.proj1.exception.ErrorResponse;
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +19,19 @@ public class UserController
 
 	private final Logger logger = LoggerFactory.getLogger(UserController.class);
 
+
+	@GetMapping(path = "/admin")
+	public ResponseEntity<ErrorResponse> getAdminDetails() {
+		logger.info("Entered into User Controller Layer");
+		return new ResponseEntity<ErrorResponse>(new ErrorResponse(HttpStatus.ACCEPTED, "getAdminDetails"), HttpStatus.ACCEPTED);
+	}
+
+	@GetMapping(path = "/user")
+	public ResponseEntity<ErrorResponse> getCurrentUser() {
+		logger.info("Entered into User Controller Layer");
+		return new ResponseEntity<ErrorResponse>(new ErrorResponse(HttpStatus.ACCEPTED, "getCurrentUser"), HttpStatus.ACCEPTED);
+	}
+
 	@GetMapping(value={"/users"})
 	public List<User> getAllUsers() {
 		logger.info("Entered into User Controller Layer");
@@ -22,17 +39,16 @@ public class UserController
 		logger.info("Records Fetched:" + results.size());
 		return results;
 	}
+//	@GetMapping(value={"/user/verifyLoginByUsername/{username}/{password}"})
+//	public boolean verifyUsername(@PathVariable String username, @PathVariable String password) {
+//		logger.info("Entered into User Controller Layer");
+//		return userService.verifyUserByUsername(username, password);
+//	}
 
-	@GetMapping(value={"/user/verifyLoginByUsername/{username}/{password}"})
-	public boolean verifyUsername(@PathVariable String username, @PathVariable String password) {
-		logger.info("Entered into User Controller Layer");
-		return userService.verifyUserByUsername(username, password);
-	}
-
-	@GetMapping(value={"/user/verifyLoginByEmail/{email}/{password}"})
-	public boolean verifyEmail(@PathVariable String email, @PathVariable String password) {
-		logger.info("Entered into User Controller Layer");
-		return userService.verifyUserByEmail(email, password);
-	}
+//	@GetMapping(value={"/user/verifyLoginByEmail/{email}/{password}"})
+//	public boolean verifyEmail(@PathVariable String email, @PathVariable String password) {
+//		logger.info("Entered into User Controller Layer");
+//		return userService.verifyUserByEmail(email, password);
+//	}
 
 }
