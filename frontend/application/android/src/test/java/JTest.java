2c667baba55e55303da5c77a6d0bf784959a5798
JLew <jlew@gmail.com>
2022-03-27 19:41:47 -0500

    made JUnit Test folder and added dependency

---
+++ frontend/application/android/src/test/java/JTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+ 
+
+
+
+}
