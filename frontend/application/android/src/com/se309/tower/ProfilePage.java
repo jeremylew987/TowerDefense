096dad058a1d6c9234c35eaac038eb6305ccebf2
JLew <jlew@gmail.com>
2022-04-01 20:27:36 -0500

    added volley calls for Profile

--- frontend/application/android/src/com/se309/tower/ProfilePage.java
+++ frontend/application/android/src/com/se309/tower/ProfilePage.java
@@ -2,6 +2,19 @@ package com.se309.tower;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ProfilePage extends AppCompatActivity {
 
@@ -9,5 +22,46 @@ public class ProfilePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+
+
+        TextView Details = findViewById(R.id.ProfileDetails);
+        Details.setText(GetProfileDetails());
+
+
+
     }
+
+    String GetProfileDetails(){
+        final RequestQueue queue = Volley.newRequestQueue(ProfilePage.this);
+        final String[] result = {""};
+        String friendaddress =  "http://coms-309-027.class.las.iastate.edu:8080/user";
+        JsonObjectRequest FriendList = new JsonObjectRequest(Request.Method.GET, friendaddress, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.i("Response: " , response.toString());
+                try {
+                    result[0] +="Username: " + response.getString("username") + "\n";
+                    result[0] +="Email: " + response.getString("email") + "\n";
+                    result[0] +="Role: " + response.getString("role") + "\n";
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(FriendList);
+        return result[0];
+    }
+
+
+
 }
