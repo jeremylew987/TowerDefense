a39a4f55ae1cefe5aaeec0a44c8f2ada09c1cc4a
jminardi <jminardi@iastate.edu>
2022-04-13 17:26:04 -0500

    Add jsonview and ignore annotations

--- backend/proj1/src/main/java/coms309/proj1/user/UserDetailsImpl.java
+++ backend/proj1/src/main/java/coms309/proj1/user/UserDetailsImpl.java
@@ -3,12 +3,15 @@ package coms309.proj1.user;
 import java.util.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonView;
+import coms309.proj1.Views;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+@JsonView(Views.Summary.class)
 public class UserDetailsImpl implements UserDetails {
-
+	
 	private User user;
 
 	public UserDetailsImpl(User user) {
@@ -16,6 +19,7 @@ public class UserDetailsImpl implements UserDetails {
 	}
 
 	@Override
+	@JsonView(Views.DetailedALL.class)
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		UserRole role = user.getRole();
 		List<SimpleGrantedAuthority> authorities = new ArrayList<>();
@@ -30,32 +34,41 @@ public class UserDetailsImpl implements UserDetails {
 	}
 
 	@Override
+	@JsonView(Views.DetailedALL.class)
 	public String getUsername() {
 		return user.getUsername();
 	}
 
+	@JsonView(Views.DetailedALL.class)
 	public String getEmail() { return user.getEmail(); }
 
+	@JsonView(Views.DetailedALL.class)
 	public Long getId() {
-		return user.getId();
+		return user.getUserId();
 	}
 
+	public User getUser() {return user;}
+
 	@Override
+	@JsonView(Views.Detailed.class)
 	public boolean isAccountNonExpired() {
 		return true;
 	}
 
 	@Override
+	@JsonView(Views.Detailed.class)
 	public boolean isAccountNonLocked() {
 		return !user.locked;
 	}
 
 	@Override
+	@JsonView(Views.Detailed.class)
 	public boolean isCredentialsNonExpired() {
 		return true;
 	}
 
 	@Override
+	@JsonView(Views.Detailed.class)
 	public boolean isEnabled() {
 		return user.enabled;
 	}
