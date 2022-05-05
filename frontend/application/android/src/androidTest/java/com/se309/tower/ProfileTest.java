978c034ba40f20af26286cee9347905f1212c469
JLew <jlew@gmail.com>
2022-05-05 15:02:16 -0500

    profile test working with delay

--- frontend/application/android/src/androidTest/java/com/se309/tower/ProfileTest.java
+++ frontend/application/android/src/androidTest/java/com/se309/tower/ProfileTest.java
@@ -27,6 +27,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static org.hamcrest.Matchers.startsWith;
 import static org.hamcrest.core.StringEndsWith.endsWith;
 @RunWith(AndroidJUnit4ClassRunner.class)
 @LargeTest   // large execution time
@@ -36,6 +37,43 @@ public class ProfileTest {
     private static final int SIMULATED_DELAY_MS = 1000;
     @Rule   // needed to launch the activity
     public ActivityScenarioRule<LoginPage> activityRule = new ActivityScenarioRule<>(LoginPage.class);
+
+
+    @Test
+    public void AProfilechcek(){
+        try {
+            Thread.sleep(SIMULATED_DELAY_MS);
+        } catch (InterruptedException e) {
+        }
+        String testString = "test";
+        String resultString = "test";
+        // Type in testString and send request
+        onView(withId(R.id.editTextTextPersonName))
+                .perform(typeText(testString), closeSoftKeyboard());
+        onView(withId(R.id.editTextTextPassword))
+                .perform(typeText(resultString), closeSoftKeyboard());
+        onView(withId(R.id.button)).perform(click());
+
+        // Put thread to sleep to allow volley to handle the request
+        try {
+            Thread.sleep(SIMULATED_DELAY_MS);
+        } catch (InterruptedException e) {
+        }
+
+        onView(withId(R.id.curUser)).perform(click());
+
+        onView(withId(R.id.ProfileDetails)).check(matches(withText(startsWith("Username: test"))));
+
+
+
+
+        onView(withId(R.id.ProfileBack)).perform(click());
+        onView(withId(R.id.logout)).perform(click());
+
+    }
+
+
+
 
 
 }
