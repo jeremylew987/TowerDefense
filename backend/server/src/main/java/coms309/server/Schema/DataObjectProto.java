a245f016f14be6b27a7e69f8607d79c1c87ffed1
jminardi <jminardi@iastate.edu>
2022-05-04 23:32:58 -0500

    UAdd damage inflicted field to enemy update protobuf

--- backend/server/src/main/java/coms309/server/Schema/DataObjectProto.java
+++ backend/server/src/main/java/coms309/server/Schema/DataObjectProto.java
@@ -78,12 +78,13 @@ public final class DataObjectProto {
       "\r\022\t\n\001y\030\002 \002(\r\022\016\n\006typeId\030\003 \002(\r\022\017\n\007ownerId\030" +
       "\004 \001(\r\"o\n\020ConnectedClients\0228\n\007clients\030\001 \003" +
       "(\0132\'.coms309.server.ConnectedClients.Cli" +
-      "ent\032!\n\006Client\022\013\n\003pid\030\001 \001(\r\022\n\n\002id\030\002 \001(\004\"\211" +
+      "ent\032!\n\006Client\022\013\n\003pid\030\001 \001(\r\022\n\n\002id\030\002 \001(\004\"\242" +
       "\001\n\010gameTick\0229\n\013enemyUpdate\030\001 \003(\0132$.coms3" +
-      "09.server.gameTick.EnemyUpdate\032B\n\013EnemyU" +
+      "09.server.gameTick.EnemyUpdate\032[\n\013EnemyU" +
       "pdate\022\017\n\007enemyId\030\001 \002(\r\022\016\n\006health\030\002 \002(\r\022\022" +
-      "\n\nattackedBy\030\003 \001(\rB*\n\025coms309.server.Sch",
-      "emaB\017DataObjectProtoP\001"
+      "\n\nattackedBy\030\003 \001(\r\022\027\n\017damageInflicted\030\004 ",
+      "\001(\rB*\n\025coms309.server.SchemaB\017DataObject" +
+      "ProtoP\001"
     };
     com.google.protobuf.Descriptors.FileDescriptor.InternalDescriptorAssigner assigner =
         new com.google.protobuf.Descriptors.FileDescriptor.    InternalDescriptorAssigner() {
@@ -144,7 +145,7 @@ public final class DataObjectProto {
     internal_static_coms309_server_gameTick_EnemyUpdate_fieldAccessorTable = new
       com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
         internal_static_coms309_server_gameTick_EnemyUpdate_descriptor,
-        new java.lang.String[] { "EnemyId", "Health", "AttackedBy", });
+        new java.lang.String[] { "EnemyId", "Health", "AttackedBy", "DamageInflicted", });
   }
 
   // @@protoc_insertion_point(outer_class_scope)
