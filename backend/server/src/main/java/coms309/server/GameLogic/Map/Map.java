5d6adc7855e563f531e9a7c073b09b00fa0bf277
jminardi <jminardi@iastate.edu>
2022-05-05 00:54:27 -0500

    Change enemies spawned back to 30

--- backend/server/src/main/java/coms309/server/GameLogic/Map/Map.java
+++ backend/server/src/main/java/coms309/server/GameLogic/Map/Map.java
@@ -65,7 +65,7 @@ public class Map {
         enemyPath = getPath(2);
         enemyQueue = new PriorityQueue<Enemy>();
         loadMap(mapId);
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < 30; i++) {
             enemyQueue.add(new Enemy(i, enemyPath.get(0), 1, 1));
         }
     }
