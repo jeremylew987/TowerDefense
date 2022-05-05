c7908b5a4bfc64a4dcdbab158921b9c0a8b8314d
JLew <jlew@gmail.com>
2022-05-05 14:47:42 -0500

    profile Tests

---
+++ frontend/application/android/src/androidTest/java/com/se309/tower/ProfileTest.java
@@ -0,0 +1,41 @@
+package com.se309.tower;
+
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.filters.LargeTest;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.isDialog;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.core.StringEndsWith.endsWith;
+@RunWith(AndroidJUnit4ClassRunner.class)
+@LargeTest   // large execution time
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+
+public class ProfileTest {
+    private static final int SIMULATED_DELAY_MS = 1000;
+    @Rule   // needed to launch the activity
+    public ActivityScenarioRule<LoginPage> activityRule = new ActivityScenarioRule<>(LoginPage.class);
+
+
+}
